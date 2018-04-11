3ab572fffd8d8af788da3d65e40262838ea1d501
Eric Mignot <eric.mignot@gmail.com>
2018-04-11 11:59:49 -0700

    react app access secured by keycloak

--- react-only/src/index.js
+++ react-only/src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Keycloak from 'keycloak-js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const keycloak = Keycloak('/keycloak.json');
+keycloak.init({ onLoad: 'login-required' }).then(function(authenticated) {
+    ReactDOM.render(<App />, document.getElementById('root'));
+    registerServiceWorker();
+}).catch(function() {
+    console.log('failed to initialize');
+});
+
+
+
